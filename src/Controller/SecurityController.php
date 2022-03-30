2d30fe27e2076a77f3eea681c228b9296366a1fc
Gauthier Jeannenot <jeannenot.gauthier@gmail.com>
2022-03-30 16:20:57 +0200

    login répareré

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SubCategoriesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,8 +13,9 @@ class SecurityController extends AbstractController
     /**
      * @Route("/login", name="app_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, SubCategoriesRepository $subCategoriesRepository): Response
     {
+        $subCategories = $subCategoriesRepository->findAll();
         if ($this->getUser()) {
             return $this->redirectToRoute('platon_main_home');
         }
@@ -23,7 +25,9 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername, 'error' => $error,
+            'subCategories' => $subCategories]);
     }
 
     /**
